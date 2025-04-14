cc2e877450ca5755b6ea5645091002a57f8c4c31
hsin0606 <jacky.hsin216@gmail.com>
2025-04-14 20:58:04 +0800

    Add files via upload

---
+++ 1.html.php
@@ -0,0 +1,10 @@
+<html>
+    <head><title>html+php</title></head>     <!-- 頁面的標題 -->
+    <body>
+        <?php echo "Hello world!"; ?>        <!-- 列印標題，echo代表列印-->
+        <h1>html</h1>        <!-- h1代表文字標題的大小 -->
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+    </body>
+</html>
